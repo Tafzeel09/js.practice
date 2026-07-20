9b7d73500d9512a7118a79541b237bf178bb115b
Taiba shabbir <tabifazl8@gmail.com>
2026-07-20 14:48:49 +0500

    Day 2: conditionals and workfloe

--- Day-1/script.js
+++ Day-1/script.js
@@ -94,3 +94,35 @@ console.log(age1 > 30 && hascard);
 console.log(age1 >18 || hascard);
 console.log(age1 < 18 || hascard);
 console.log(!hascard);
+let year = 20;
+if (year <= 19) {
+    console.log("You are eligible to vote.");
+} 
+else{
+    console.log("You are not eligible to vote.");
+}
+let sdmarks = 70;
+if (sdmarks >= 90){
+    console.log("grade A");
+}
+else if (sdmarks >= 80){
+    console.log("grade B");
+}
+else {
+    console.log("grade C");
+}
+let day = 3;
+switch (day) {
+    case 1:
+    console.log("Monday");
+    break;
+    case 2:
+    console.log("Tuesday");
+    break;
+    case 3:
+    console.log("Wednesday");
+    break;
+    default:
+        console.log("Invalid day");
+    
+}
