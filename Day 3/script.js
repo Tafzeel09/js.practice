cad623112f4a5d296a47fe148e2f38a67d31a89a
Taiba shabbir <tabifazl8@gmail.com>
2026-07-21 12:59:58 +0500

    added day 2 and 3 javascript practice

---
+++ Day 3/script.js
@@ -0,0 +1,30 @@
+for (let i = 1; i <= 5; i++){
+    console.log(i)}
+for (let i = 1; i<= 10; i++){
+console.log(i)
+}
+for(let i =10; i>=1; i--){
+    console.log(i)
+}for(let i=2; i<= 20; i+=2){
+    console.log(i);
+}
+for(let i=1;  i<= 20; i++){
+    console.log("5 x " + i + " = " +(5 * i));
+}
+for(let i=1; i<= 10; i++){
+    console.log("2 x " + i + " = " + (2 * i));
+}
+let num = 5;
+do {
+    console.log(num);
+    num--;
+} while (num >= 1);
+let nums = 20;
+while(nums >= 1){
+    console.log(nums);nums--;
+}
+let count = 5;
+do{
+    console.log(count);
+    count--;
+} while(count >= 1);
