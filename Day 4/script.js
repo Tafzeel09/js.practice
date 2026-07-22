4f3ba03b05be7983e2f46ba8eaedd0bae68368b7
Taiba shabbir <tabifazl8@gmail.com>
2026-07-22 19:16:57 +0500

    day 4: functions, parameters and returns

---
+++ Day 4/script.js
@@ -0,0 +1,30 @@
+function sayHello(){
+    console.log("Hello Javascript");
+}sayHello();
+function welcome(){
+    console.log("welcome to day 3");
+}welcome();
+welcome();
+function myName(){
+    console.log("Taiba");
+}myName();
+function student(name)
+{
+    console.log("student:" + name);}
+    student("ali");
+function multiply(a, b)
+{console.log(a * b);}
+multiply(10, 2);
+function minus(a, b)
+{return a-b;}
+console.log(minus(10,4));
+function fullName(firstName,lastName)
+{return firstName + " " + lastName;}
+console.log(fullName("Taiba", "Shabbir"));
+function cube(num){
+return num * num * num;}
+console.log(cube(3));
+function isEven(num){
+    return num % 2 === 0;
+}console.log(isEven(2));
+console.log(isEven(7));
