5467a84d5a2f6f3e4cd36a2a9b0520ec740c8b69
Taiba shabbir <tabifazl8@gmail.com>
2026-07-23 10:49:43 +0500

    day 5 arrays practice

---
+++ Day 5/script.js
@@ -0,0 +1,20 @@
+let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+console.log(fruits);
+let fruitss =["apple", "banana", "cherry", "date", "elderberry"];
+fruitss.push("fig");
+console.log(fruitss);
+let colour =["red", "green"
+]
+colour.push("blue");
+console.log(colour);
+let colors =["red", "yellow"]
+colors.pop();
+console.log(colors);
+let fruit =["apple", "banana"]
+fruit.shift();
+console.log(fruit);
+let fr = ["apple"]
+fr.unshift("mango");
+console.log(fr);
+console.log(fr.length);
+
